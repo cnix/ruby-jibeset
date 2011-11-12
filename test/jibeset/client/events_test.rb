9eb20b4d5a43f876dc150de38b7e025356439bf1
David Conner <dconner.pro@gmail.com>
2011-11-12 11:28:41 -0500

    Configured gem to use MiniTest

--- test/jibeset/client/events_test.rb
+++ test/jibeset/client/events_test.rb
@@ -3,20 +3,38 @@ require File.expand_path('../../../test_helper', __FILE__)
 class EventTest < Jibeset::Test
   
   def setup
-    @client = Jibeset.client
-    @client.stubs = Faraday::Adapter::Test::Stubs.new
+    @jibeset = Jibeset.client
+    @jibeset.stubs = Faraday::Adapter::Test::Stubs.new
   end
 
   def test_events_should_return_events
-    @client.stubs.get('/events.json') { [200, {}, '{"events":[]}'] } 
-    assert_not_nil @client.events
+    @jibeset.stubs.get('/events.json') { [200, {}, '{"events":[]}'] } 
+    refute_nil @jibeset.events
+  end
+
+  def test_event_entries_should_return_entries
+
   end
 
   def test_create_event_should_create_event
     payload = { :name => 'tybee', :event_type => 'series' }
-    @client.stubs.post('/events.json')  { [200,{},Yajl.dump(payload)] }
-    response = @client.create_event(payload)
+    @jibeset.stubs.post('/events.json')  { [200,{},Yajl.dump(payload)] }
+    client = @jibeset.create_event(payload)
     
-    assert_equal  Yajl.dump(payload), response 
+    assert_equal Yajl.dump(payload), client
   end
+
+  def test_update_event_should_update_event
+
+  end
+
+  def test_destroy_event_should_destroy_event
+
+  end
+
+  def test_register_entry
+
+  end
+
+
 end
